b3adaaf12b7e660c018d3da61d0afa6ed12834c2
Peter Hendriks <peter@the-underground.nl>
2020-12-15 23:01:18 +0100

    Quick and Dirty light implementation

--- shifter/__init__.py
+++ shifter/__init__.py
@@ -3,7 +3,13 @@ from time import sleep
 
 
 class shifter595:
-    def __init__(self):
+    def __init__(self, chain=2):
+        self.chain = chain  # number of chips in a row
+
+        self.registers = []
+        for i in range(0, self.chain):
+            self.registers.append([0, 0, 0, 0, 0, 0, 0, 0])
+
         self.latch_pin = DigitalOutputDevice(16)
         self.clock_pin = DigitalOutputDevice(20)
         self.data_pin = DigitalOutputDevice(21)
@@ -16,15 +22,15 @@ class shifter595:
         self.clock_pin.off()
         sleep(self.pause)
 
-    def set_value(self, value):
-        for i in range(24):
-            bitwise = 0x800000 >> i
-            bit = bitwise & value
-            if bit == 0:
-                self.data_pin.off()
-            else:
-                self.data_pin.on()
-            self.tick()
+    def set_value(self):
+        for i in range(0, self.chain):
+            for n in range(0, 8): # 74hc595 has 8 outputs
+                bit = self.registers[i][n]
+                if bit == 0:
+                    self.data_pin.off()
+                else:
+                    self.data_pin.on()
+                self.tick()
 
     def latch(self):
         self.latch_pin.off()
