8e9f9aa5b477d2c869ab26186dab4185b5c2ee40
Peter Hendriks <peter@the-underground.nl>
2020-12-17 10:16:30 +0100

    Quick and Dirty light implementation

--- 595test.py
+++ 595test.py
@@ -1,18 +1,20 @@
 from shifter import shifter595
 from time import sleep
 
-pause = 0.5
+test_runs = 10
+pause = 0.1
 s = shifter595()
 
 s.clear_all_registers()
 sleep(pause)
 
-for i in range(0, 2):
-    for n in range(0, 8):
-        s.clear_all_registers()
-        s.registers[i][n] = 1
-        s.load_values()
-        s.latch()
-        sleep(pause)
+for x in range(0, test_runs):
+    for i in range(0, 2):
+        for n in range(0, 8):
+            s.clear_all_registers()
+            s.registers[i][n] = 1
+            s.load_values()
+            s.latch()
+            sleep(pause)
 
 
