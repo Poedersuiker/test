2cbc2189ea6e291d7251095a9372797eb29ea42a
Peter Hendriks <peter@the-underground.nl>
2021-02-27 15:10:17 +0100

    New Test file

--- stepper motor.py
+++ stepper motor.py
@@ -34,11 +34,19 @@ def backwards(delay, steps):
     while i in range(0, steps):
         setStep(1, 0, 0, 1)
         time.sleep(delay)
-        setStep(0, 1, 0, 1)
+        setStep(0, 0, 0, 1)
+        time.sleep(delay)
+        setStep(0, 0, 1, 1)
+        time.sleep(delay)
+        setStep(0, 0, 1, 0)
         time.sleep(delay)
         setStep(0, 1, 1, 0)
         time.sleep(delay)
-        setStep(1, 0, 1, 0)
+        setStep(0, 1, 0, 0)
+        time.sleep(delay)
+        setStep(1, 1, 0, 0)
+        time.sleep(delay)
+        setStep(1, 0, 0, 0)
         time.sleep(delay)
         i += 1
 
