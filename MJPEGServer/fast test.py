fc379d998eab08d8506029cf760f9b1995a5c0a4
Peter Hendriks <peter@the-underground.nl>
2021-04-30 22:16:40 +0200

    MJPEG test server

--- MJPEGServer/fast test.py
+++ MJPEGServer/fast test.py
@@ -1,28 +1,18 @@
 from time import sleep
 import datetime
 import cv2
-import numpy
 
 
 video_capture = cv2.VideoCapture(-1)
 filename = "./last.jpg"
+ret, frame = video_capture.read()
 
 while video_capture.isOpened():
     # Send picture
     sleep(0.1)
     now = datetime.datetime.now()
     watermark = now.strftime("%Y-%m-%d %H:%M:%S")
-    try:
-        ret, frame = video_capture.read()
-    except cv2.error as e:
-        print(e)
-        frame = numpy.zeros((320, 280, 3), numpy.uint8)
-    except Exception as e:
-        print(e)
-        frame = numpy.zeros((320, 280, 3), numpy.uint8)
-    else:
-        print('OpenCV2 error with capture device, but no Exception')
-        frame = numpy.zeros((320, 280, 3), numpy.uint8)
+    ret, frame = video_capture.read()
 
     font = cv2.FONT_HERSHEY_SIMPLEX
     cv2.putText(frame, str(watermark), (0, -40), font, 5, (255, 255, 255), 3)
